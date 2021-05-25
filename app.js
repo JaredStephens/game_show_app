e2fb425b1b9e0ec5ba57847b4c40ee4cb5638583
JaredStephens <jbstephens00@gmail.com>
2021-05-25 11:35:30 -0400

    editing variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // DECLARATIONS
-const qwerty = document.getElementById('qwerty')';
-const phrase = document.getElementById('phrase');
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
 const missed = 0;
-const startGame = document.getElementById("bnt_reset")
+const startGame = document.getElementById("bnt_reset");
 // PHRASES
+const phrasesArray = [];
