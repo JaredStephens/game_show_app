443984107156eedb9024283a9bf39bc330429efa
JaredStephens <jbstephens00@gmail.com>
2021-05-27 15:45:45 -0400

    adding phrases in array/ random phrase function

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const missed = 0;
 const startGame = document.getElementById("bnt_reset");
-// PHRASES
-const phrasesArray = [];
+// RANDOM PHRASES
+const phrasesArray = [
+	"Its Clobberin Time",
+	"By Odins Beard",
+	"We have a Hulk",
+	"Im the best at what I do",
+	"Hulk Smash",
+];
+// START BUTOTN
+
+// RANDOM PHRASE FUNCTION
+function getRandomPhraseArray(phrasesArray) {
+	return phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
+}
