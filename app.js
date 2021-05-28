fc40430708f0074fe921147a383107659d43e67d
JaredStephens <jbstephens00@gmail.com>
2021-05-28 16:00:14 -0400

    keyboard event listener function

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // DECLARATIONS
-
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 let missed = 0;
 const startGame = document.querySelector(".btn__reset");
 const overlay = document.getElementById("overlay");
-// RANDOM PHRASES
 
+// RANDOM PHRASES
 const phrasesArray = [
 	"Its Clobberin Time",
 	"By Odins Beard",
@@ -14,19 +13,19 @@ const phrasesArray = [
 	"Im the best at what I do",
 	"Hulk Smash",
 ];
-// HIDE OVERLAY
 
+// HIDE OVERLAY
 startGame.addEventListener("click", function () {
 	overlay.style.display = "none";
 });
-// RANDOM PHRASE FUNCTION
 
+// RANDOM PHRASE FUNCTION
 function getRandomPhraseArray(arr) {
 	return phrasesArray[Math.floor(Math.random() * arr.length)].split("");
 }
 randomPhrase = getRandomPhraseArray(phrasesArray);
-//GAME DISPLAY
 
+//GAME DISPLAY
 function addPhraseToDisplay(arr) {
 	for (i = 0; i < arr.length; i++) {
 		let li = document.createElement("li");
@@ -40,3 +39,34 @@ function addPhraseToDisplay(arr) {
 	}
 }
 addPhraseToDisplay(randomPhrase);
+
+// CHECK LETTER FUNCTION
+function checkLetter(button) {
+	const letter = document.querySelectorAll(".letter");
+	let letterFound = null;
+	for (i = 0; i < letter.length; i++) {
+		if (button === letter[i].textContent.toLowerCase()) {
+			letter[li].classList.add("show");
+			letter[i].style.transition = "1s ease-in";
+			letterFound = true;
+		}
+		return letterFound;
+	}
+}
+
+// KEYBOARD
+qwerty.addEventListener("click", (e) => {
+	if (e.target.tagName === "button") {
+		e.target.className = "chosen";
+		e.target.disabled = true;
+		const match = checkLetter(e.target.textContent.toLowerCase());
+
+		if (match === null) {
+			missed++;
+			const lostLife = 5 - missed;
+			const lives = document.querySelectorAll("img");
+			lives[lostLife].setAttribute("src", "images/lostHeart.png");
+		}
+		checkWin();
+	}
+});
